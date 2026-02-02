9ba93ce7db404f9b3307bc9e116ad8a8090a08f5
Alilux <aliluxxxxx@gmail.com>
2026-02-02 15:50:06 +0100

    Added CMake files

---
+++ scripts/dependencies.cmake
@@ -0,0 +1,22 @@
+# Include dirs
+set(SANDBOX_INCLUDE_DIRS
+	${CMAKE_CURRENT_SOURCE_DIR}/src
+	${CMAKE_SOURCE_DIR}/sandbox-core/src
+)
+
+# Library dirs
+set(SANDBOX_LIB_DIRS
+	"sandbox-core"
+)
+
+# Submodule libraries
+list(APPEND SANDBOX_INCLUDE_DIRS
+	${CMAKE_SOURCE_DIR}/sandbox-core/libs/glad/include
+	${CMAKE_SOURCE_DIR}/sandbox-core/libs/glfw/include
+	${CMAKE_SOURCE_DIR}/sandbox-core/libs/glm
+	${CMAKE_SOURCE_DIR}/sandbox-core/libs/spdlog/include
+)
+list(APPEND SANDBOX_LIB_DIRS
+	glfw
+	glad
+)
