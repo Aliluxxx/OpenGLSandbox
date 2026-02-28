fea2f74571735f3dfe1969e2577d3c22b86aa000
Alilux <aliluxxxxx@gmail.com>
2026-02-28 14:24:31 +0100

    Set maximum OpenGL version to 4.5

--- sandbox-core/src/Platform/GLFW/GLFWWindow.cpp
+++ sandbox-core/src/Platform/GLFW/GLFWWindow.cpp
@@ -56,7 +56,7 @@ namespace sb {
 		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_FALSE);
 #else
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 #endif
