d70f7b7043e291e91dba23178f504719473c8152
Alilux <aliluxxxxx@gmail.com>
2026-03-11 13:55:55 +0100

    Removed VAO bind after creation

--- sandbox-core/src/Core/Renderer/VertexArray.cpp
+++ sandbox-core/src/Core/Renderer/VertexArray.cpp
@@ -7,7 +7,6 @@ namespace sb {
 	VertexArray::VertexArray() {
 
 		glGenVertexArrays(1, &m_RendererID);
-		glBindVertexArray(m_RendererID);
 	}
 
 	VertexArray::~VertexArray() {
@@ -23,20 +22,26 @@ namespace sb {
 
 		glBindVertexArray(m_RendererID);
 		glBindBuffer(GL_ARRAY_BUFFER, buffer->GetRendererID());
-		Uint32 index = 0;
 		const auto& layout = buffer->GetLayout();
+		Uint32 index = layout.GetIndex();
 		for (const auto& element : layout) {
 
-			glEnableVertexAttribArray(index);
-			glVertexAttribPointer(index,
-				element.GetComponentCount(),
-				Utils::ShaderDataTypeToOpenGLType(element.Type),
-				element.Normalized ? GL_TRUE : GL_FALSE,
-				layout.GetStride(),
-				(const void*)element.Offset
-			);
-			index++;
+			for (Uint32 i = 0; i < element.GetVertexAttributeCount(); i++) {
+
+				glEnableVertexAttribArray(index);
+				glVertexAttribDivisor(index, element.Divisor);
+				glVertexAttribPointer(index,
+					element.GetComponentCount(),
+					Utils::ShaderDataTypeToOpenGLType(element.Type),
+					element.Normalized ? GL_TRUE : GL_FALSE,
+					layout.GetStride(),
+					(const void*)(element.Offset + std::size_t(element.GetComponentCount() * element.GetVertexAttributeCount() * i))
+				);
+				index++;
+			}
 		}
+
+		glBindVertexArray(0);
 	}
 
 	void VertexArray::SetIndexBuffer(const Ref<IndexBuffer>& buffer) {
@@ -44,6 +49,7 @@ namespace sb {
 		glBindVertexArray(m_RendererID);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer->GetRendererID());
 		m_IndexBuffer = buffer;
+		glBindVertexArray(0);
 	}
 
 	Ref<VertexArray> VertexArray::Create() {
