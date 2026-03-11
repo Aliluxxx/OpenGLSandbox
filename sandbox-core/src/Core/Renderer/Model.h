036c5d6117026bab47952e64e3c8307c628de247
Alilux <aliluxxxxx@gmail.com>
2026-03-11 14:29:14 +0100

    Changed method's position in the code

--- sandbox-core/src/Core/Renderer/Model.h
+++ sandbox-core/src/Core/Renderer/Model.h
@@ -18,10 +18,10 @@ namespace sb {
 		Model(const std::filesystem::path& path);
 		Model(const Model& other);
 
-		inline const std::vector<Mesh>& GetMeshes() const { return m_Meshes; }
-
 		static Ref<Model> Create(const std::filesystem::path& path);
 		static Ref<Model> Create(const Ref<Model>& other);
+
+		inline const std::vector<Mesh>& GetMeshes() const { return m_Meshes; }
 
 	private:
 
