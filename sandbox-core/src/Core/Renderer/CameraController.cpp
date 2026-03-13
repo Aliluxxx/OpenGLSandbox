80677e15694d722e06ff4f8ca83ab62eb72b5c26
Alilux <aliluxxxxx@gmail.com>
2026-03-13 14:09:26 +0100

    Added orthographic camera controller

--- sandbox-core/src/Core/Renderer/CameraController.cpp
+++ sandbox-core/src/Core/Renderer/CameraController.cpp
@@ -1,13 +1,94 @@
-#include "Core/Events/ApplicationEvent.h"
-#include "Core/Events/MouseEvent.h"
 #include "Core/Base/Input.h"
 
 #include "Core/Renderer/CameraController.h"
 
-namespace sb{
+namespace sb {
+
+	// Orthographic
+
+	OrthographicCameraController::OrthographicCameraController(float aspect_ratio, float zoom_level, float near_plane, float far_plane)
+		:
+		m_Camera(-aspect_ratio * zoom_level, aspect_ratio * aspect_ratio, -aspect_ratio, zoom_level, near_plane, far_plane),
+		m_AspectRatio(aspect_ratio),
+		m_ZoomLevel(zoom_level)
+
+	{}
+
+	OrthographicCameraController::OrthographicCameraController(float near_plane, float far_plane)
+		:
+		m_Camera(-1.0f, 1.0f, -1.0f, 1.0f, near_plane, far_plane)
+
+	{}
+
+	void OrthographicCameraController::OnUpdate(Time ts) {
+
+		const float deltaTime = ts.AsSeconds();
+
+		Vector3f position = m_Camera.GetPosition();
+		float rotation = m_Camera.GetRotation();
+
+		if (Input::IsKeyPressed(Key::W))
+			position.y += m_TranslationSpeed * deltaTime;
+
+		else if (Input::IsKeyPressed(Key::S))
+			position.y -= m_TranslationSpeed * deltaTime;
+
+		if (Input::IsKeyPressed(Key::A))
+			position.x -= m_TranslationSpeed * deltaTime;
+
+		else if (Input::IsKeyPressed(Key::D))
+			position.x += m_TranslationSpeed * deltaTime;
+
+		if (Input::IsKeyPressed(Key::Q))
+			rotation += m_RotationSpeed * deltaTime;
+
+		if (Input::IsKeyPressed(Key::E))
+			rotation -= m_RotationSpeed * deltaTime;
+
+		m_Camera.SetPosition(position);
+		m_Camera.SetRotation(rotation);
+	}
+
+	void OrthographicCameraController::OnEvent(Event& e) {
+
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<MouseScrolledEvent>(SB_BIND_EVENT_FN(OrthographicCameraController::OnMouseScrolled));
+	}
+
+	void OrthographicCameraController::OnResize(float width, float height) {
+
+		m_AspectRatio = width / height;
+		m_Camera.SetRectangle(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+	}
+
+	void OrthographicCameraController::SetTranslationSpeed(float speed) {
+
+		m_TranslationSpeed = speed;
+	}
+
+	void OrthographicCameraController::SetRotationSpeed(float speed) {
+
+		m_RotationSpeed = speed;
+	}
+
+	void OrthographicCameraController::SetZoomSensitivity(float sensitivity) {
+
+		m_ZoomSensitivity = sensitivity;
+	}
+
+	bool OrthographicCameraController::OnMouseScrolled(MouseScrolledEvent& e) {
+
+		m_ZoomLevel -= e.GetYOffset() * m_ZoomSensitivity * glm::abs(m_ZoomLevel);
+		m_ZoomLevel = m_ZoomLevel < 0.0f ? m_ZoomSensitivity : m_ZoomLevel;
+		m_Camera.SetRectangle(-m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel);
+
+		return true;
+	}
+
+	// Fly
 
 	FlyCameraController::FlyCameraController(float field_of_view, float aspect_ratio, float near_plane, float far_plane)
-		: m_Camera(field_of_view, aspect_ratio, near_plane, far_plane), m_Speed(10.0f), m_Sensitivity(0.1f)
+		: m_Camera(field_of_view, aspect_ratio, near_plane, far_plane)
 
 	{}
 
@@ -22,7 +103,7 @@ namespace sb{
 		const Vector3f& front = m_Camera.GetFront();
 		const Vector3f& up = m_Camera.GetUp();
 
-		if (Input::IsKeyPressed(Key::Q))
+		if (Input::IsKeyPressed(Key::LeftShift))
 			m_Speed = 1.0f;
 		else
 			m_Speed = 10.0f;
@@ -45,7 +126,6 @@ namespace sb{
 	void FlyCameraController::OnEvent(Event& e) {
 
 		EventDispatcher dispatcher(e);
-		dispatcher.Dispatch<WindowResizeEvent>(SB_BIND_EVENT_FN(FlyCameraController::OnWindowResized));
 		if (!m_Disabled)
 			dispatcher.Dispatch<MouseMovedEvent>(SB_BIND_EVENT_FN(FlyCameraController::OnMouseMoved));
 	}
@@ -69,13 +149,6 @@ namespace sb{
 
 		m_Disabled = disable;
 		m_FirstMouse = true;
-	}
-
-	bool FlyCameraController::OnWindowResized(WindowResizeEvent& e) {
-
-		OnResize((float)e.GetWidth(), (float)e.GetHeight());
-
-		return true;
 	}
 
 	bool FlyCameraController::OnMouseMoved(MouseMovedEvent& e) {
--- sandbox-core/src/Core/Renderer/CameraController.h
+++ sandbox-core/src/Core/Renderer/CameraController.h
@@ -3,6 +3,7 @@
 #include "Core/Base/Base.h"
 #include "Core/Base/Time.h"
 #include "Core/Events/Event.h"
+#include "Core/Events/MouseEvent.h"
 #include "Core/Renderer/Camera.h"
 
 namespace sb {
@@ -16,34 +17,75 @@ namespace sb {
 		virtual void OnResize(float width, float height) = 0;
 	};
 
-	class FlyCameraController : public CameraController {
+	// Orthographic
+
+	class OrthographicCameraController : public CameraController {
 
 	public:
 
-		FlyCameraController(float field_of_view, float aspect_ratio, float near_plane, float far_plane);
+		OrthographicCameraController(float aspect_ratio, float zoom_level, float near_plane, float far_plane);
+		OrthographicCameraController(float near_plane = -1.0f, float far_plane = 1.0f);
+		OrthographicCameraController() = default;
 
 		virtual void OnUpdate(Time ts) override;
 		virtual void OnEvent(Event& e) override;
 		virtual void OnResize(float width, float height) override;
 
+		inline const OrthographicCamera& GetCamera() const { return m_Camera; }
+		inline sb::Matrix4f GetViewProjectionMatrix() { return m_Camera.GetProjectionMatrix() * m_Camera.GetViewMatrix(); }
+
+		void SetTranslationSpeed(float speed);
+		inline float GetTranslationSpeed() const { return m_TranslationSpeed; }
+		void SetRotationSpeed(float speed);
+		inline float GetRotationSpeed() const { return m_RotationSpeed; }
+		void SetZoomSensitivity(float sensitivity);
+		inline float GetZoomSensitivity() const { return m_ZoomSensitivity; }
+
+	protected:
+
+		OrthographicCamera m_Camera;
+		float m_AspectRatio = 1.0f;
+		float m_ZoomLevel = 1.0f;
+		float m_TranslationSpeed = 1.0f;
+		float m_RotationSpeed = 10.0f;
+		float m_ZoomSensitivity = 0.1f;
+
+	private:
+
+		bool OnMouseScrolled(MouseScrolledEvent& e);
+	};
+
+	// Fly
+
+	class FlyCameraController : public CameraController {
+
+	public:
+
+		FlyCameraController(float field_of_view, float aspect_ratio, float near_plane, float far_plane);
+		FlyCameraController() = default;
+
+		virtual void OnUpdate(Time ts) override;
+		virtual void OnEvent(Event& e) override;
+		virtual void OnResize(float width, float height) override;
+
+		inline const FlyCamera& GetCamera() const { return m_Camera; }
+		inline sb::Matrix4f GetViewProjectionMatrix() { return m_Camera.GetProjectionMatrix() * m_Camera.GetViewMatrix(); }
+
 		void SetSpeed(float speed);
 		inline float GetSpeed() const { return m_Speed; }
 		void SetSensitivity(float sensitivity);
 		inline float GetSensitivity() const { return m_Sensitivity; }
-		inline sb::Matrix4f GetViewProjectionMatrix() { return m_Camera.GetProjectionMatrix() * m_Camera.GetViewMatrix(); }
 		void SetCameraMovementDisabled(bool disable);
-		inline const FlyCamera& GetCamera() const { return m_Camera; }
 
 	protected:
 
-		bool OnWindowResized(WindowResizeEvent& e);
-		bool OnMouseMoved(MouseMovedEvent& e);
-
 		FlyCamera m_Camera;
-		float m_Speed;
-		float m_Sensitivity;
+		float m_Speed = 10.0f;
+		float m_Sensitivity = 0.1f;
 
 	private:
+
+		bool OnMouseMoved(MouseMovedEvent& e);
 
 		bool m_FirstMouse = true;
 		float m_LastXPos = 0.0f;
